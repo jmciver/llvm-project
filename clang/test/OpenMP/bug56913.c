4e763e3b0be1d722a5cf8dd12c0446b1854de02a
John McIver <john.mciver.iii@gmail.com>
2024-01-23 11:13:33 -0700

    [clang][OpenMP] Run automatic test update

--- clang/test/OpenMP/bug56913.c
+++ clang/test/OpenMP/bug56913.c
@@ -16,13 +16,22 @@ void loop(int n) {
 // CHECK-SAME: (i32 noundef [[N:%.*]]) local_unnamed_addr #[[ATTR0:[0-9]+]] {
 // CHECK-NEXT:  entry:
 // CHECK-NEXT:    [[J:%.*]] = alloca i32, align 4
+// CHECK-NEXT:    [[SUB2:%.*]] = add nsw i32 [[N]], -1
 // CHECK-NEXT:    [[CMP:%.*]] = icmp sgt i32 [[N]], 0
 // CHECK-NEXT:    br i1 [[CMP]], label [[SIMD_IF_THEN:%.*]], label [[SIMD_IF_END:%.*]]
 // CHECK:       simd.if.then:
-// CHECK-NEXT:    [[TMP0:%.*]] = load i32, ptr @j, align 4, !tbaa [[TBAA2:![0-9]+]]
+// CHECK-NEXT:    [[TMP0:%.*]] = load i32, ptr @j, align 4, !tbaa [[TBAA2:![0-9]+]], !freeze_bits [[FREEZE_BITS6:![0-9]+]]
 // CHECK-NEXT:    call void @llvm.lifetime.start.p0(i64 4, ptr nonnull [[J]]) #[[ATTR2:[0-9]+]]
-// CHECK-NEXT:    store ptr [[J]], ptr @u, align 8, !tbaa [[TBAA6:![0-9]+]], !llvm.access.group [[ACC_GRP8:![0-9]+]]
-// CHECK-NEXT:    [[INC_LE:%.*]] = add i32 [[TMP0]], [[N]]
+// CHECK-NEXT:    store ptr [[J]], ptr @u, align 8, !tbaa [[TBAA7:![0-9]+]], !llvm.access.group [[ACC_GRP9:![0-9]+]]
+// CHECK-NEXT:    br label [[OMP_INNER_FOR_BODY:%.*]]
+// CHECK:       omp.inner.for.body:
+// CHECK-NEXT:    [[DOTOMP_IV_026:%.*]] = phi i32 [ 0, [[SIMD_IF_THEN]] ], [ [[ADD12:%.*]], [[OMP_INNER_FOR_BODY]] ]
+// CHECK-NEXT:    [[ADD12]] = add i32 [[DOTOMP_IV_026]], 1
+// CHECK-NEXT:    [[CMP8_NOT:%.*]] = icmp ugt i32 [[ADD12]], [[SUB2]]
+// CHECK-NEXT:    br i1 [[CMP8_NOT]], label [[OMP_INNER_FOR_END:%.*]], label [[OMP_INNER_FOR_BODY]], !llvm.loop [[LOOP10:![0-9]+]]
+// CHECK:       omp.inner.for.end:
+// CHECK-NEXT:    [[ADD11:%.*]] = add i32 [[TMP0]], 1
+// CHECK-NEXT:    [[INC_LE:%.*]] = add i32 [[ADD11]], [[DOTOMP_IV_026]]
 // CHECK-NEXT:    store i32 [[INC_LE]], ptr [[J]], align 4, !tbaa [[TBAA2]]
 // CHECK-NEXT:    store i32 [[INC_LE]], ptr @j, align 4, !tbaa [[TBAA2]]
 // CHECK-NEXT:    call void @llvm.lifetime.end.p0(i64 4, ptr nonnull [[J]]) #[[ATTR2]]
