840a20e8532bcf2ca6128d11b6458afdde3e5107
John McIver <john.mciver.iii@gmail.com>
2024-04-24 16:47:50 -0600

    [SROA][u2p] Adjusting out of bound to use conditional freeze_bits

--- llvm/lib/Transforms/Scalar/SROA.cpp
+++ llvm/lib/Transforms/Scalar/SROA.cpp
@@ -249,7 +249,7 @@ public:
             sys::Process::GetEnv("DEV_SROA_REPLACE_MAXIMUM"))
       MaxNumberOfReplacements = std::stoi(*sroaReplaceMaximum);
     else
-      MaxNumberOfReplacements = -3;
+      MaxNumberOfReplacements = 123;
   }
 
   /// Main run method used by both the SROAPass and by the legacy pass.
@@ -2955,7 +2955,8 @@ private:
       useFreezeBits = true;
       ++NumberOfReplacements;
     } else {
-      useFreezeBits = false;
+      useFreezeBits = loadHasFreezeBits(LI);
+      // useFreezeBits = false;
     }
     return useFreezeBits;
   }
