c1b54d2f89d1ae23e6b23189a79d696353dd5acf
John McIver <john.mciver.iii@gmail.com>
2024-10-21 11:23:51 -0600

    [clang][wip] Add freeze poison emission to local pointer allocation

--- clang/lib/CodeGen/CGDecl.cpp
+++ clang/lib/CodeGen/CGDecl.cpp
@@ -1597,7 +1597,8 @@ CodeGenFunction::EmitAutoVarAlloca(const VarDecl &D) {
       } else {
         assert(!emission.useLifetimeMarkers());
       }
-      if ((allocaTy->isIntegerTy() || allocaTy->isFloatingPointTy()) &&
+      if ((allocaTy->isIntegerTy() || allocaTy->isFloatingPointTy() ||
+           allocaTy->isPointerTy()) &&
           HaveInsertPoint()) {
         auto Freeze = Builder.CreateFreeze(llvm::PoisonValue::get(allocaTy),
                                            "freeze.poison");
