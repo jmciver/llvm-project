547f4c12bb6131923fec1362a543d43c6f828f60
John McIver <john.mciver.iii@gmail.com>
2025-04-29 14:07:05 -0600

    [llvm] Add freeze-bits support to isGuaranteedNotToBeUndefOrPoison

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -7666,7 +7666,8 @@ static bool isGuaranteedNotToBeUndefOrPoison(
   if (auto *I = dyn_cast<LoadInst>(V))
     if (I->hasMetadata(LLVMContext::MD_noundef) ||
         I->hasMetadata(LLVMContext::MD_dereferenceable) ||
-        I->hasMetadata(LLVMContext::MD_dereferenceable_or_null))
+        I->hasMetadata(LLVMContext::MD_dereferenceable_or_null) ||
+        I->hasMetadata(LLVMContext::MD_freeze_bits))
       return true;
 
   if (programUndefinedIfUndefOrPoison(V, !includesUndef(Kind)))
