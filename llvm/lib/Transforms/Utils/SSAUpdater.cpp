655966fdba1b88b603a0d12fec2e63747312ecee
John McIver <john.mciver.iii@gmail.com>
2025-10-21 13:27:14 -0400

    [SSAUpdater] Add freeze conditionally to load rewrite

--- llvm/lib/Transforms/Utils/SSAUpdater.cpp
+++ llvm/lib/Transforms/Utils/SSAUpdater.cpp
@@ -16,6 +16,7 @@
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/TinyPtrVector.h"
 #include "llvm/Analysis/InstructionSimplify.h"
+#include "llvm/Analysis/MemoryBuiltins.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/CFG.h"
 #include "llvm/IR/Constants.h"
@@ -25,6 +26,7 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Use.h"
 #include "llvm/IR/Value.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
@@ -475,6 +477,10 @@ void LoadAndStorePromoter::run(const SmallVectorImpl<Instruction *> &Insts) {
 
     // Avoid assertions in unreachable code.
     if (NewVal == ALoad) NewVal = PoisonValue::get(NewVal->getType());
+    if (loadHasFreezeBits(ALoad)) {
+      llvm::IRBuilder<> Builder(ALoad);
+      NewVal = Builder.CreateFreeze(NewVal);
+    }
     ALoad->replaceAllUsesWith(NewVal);
     ReplacedLoads[ALoad] = NewVal;
   }
